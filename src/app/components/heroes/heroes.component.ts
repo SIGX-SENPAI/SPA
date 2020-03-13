b91b4e7e9f89fe19cc79a59cb535dd16dcc500e6
SIGX-SENPAI <christian2011b@hotmail.com>
2020-03-13 02:19:16 -0500

    First Commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: any[] = [];
+  constructor( private _heroesService: HeroesService ) {
+    console.log('constructor');
+   }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
+  }
+
+}
